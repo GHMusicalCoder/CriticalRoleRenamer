fad36cbb5a90f024721f6ed7fdf4c0b1b33974b3
MusicalCoder <github@musicalcoder.com>
2016-08-27 00:40:20 -0400

    Added additional items to regex, as titles with commas and periods were breaking

--- critrolerename.py
+++ critrolerename.py
@@ -5,11 +5,11 @@ import shutil
 def main():
     path = os.path.join('/home', 'chris', 'Work')
     os.chdir(path)
-    regex = re.compile('([\w\s&]+)\s-\s[\w\s]+Episode\s(\d+)')
+    regex = re.compile('([\w\s&\.,]+)\s-\s[\w\s]+Episode\s(\d+)')
     for file in os.listdir(path):
         match = regex.search(file)
         if match is not None:
-            #shutil.move(file, 'Critical Role - S01E{:0>2} - {}.mp3'.format(match.group(2), match.group(1)))
+            #shutil.move(file, 'Critical Role - S01E{:0>2} - {}.mp3'.format(match.group(2), match.group(1).title()))
             print('Critical Role - S01E{:0>2} - {}.mp3'.format(match.group(2), match.group(1).title()))
 
 
