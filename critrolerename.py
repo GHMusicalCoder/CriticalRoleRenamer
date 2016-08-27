1cb6dff791b5f3d0de1837ec2ad85fd760f7211a
MusicalCoder <github@musicalcoder.com>
2016-08-27 00:12:42 -0400

    Initial Commit

---
+++ critrolerename.py
@@ -0,0 +1,18 @@
+import os
+import re
+import shutil
+
+def main():
+    path = os.path.join('/home', 'chris', 'Work')
+    os.chdir(path)
+    regex = re.compile('([\w\s&]+)\s-\s[\w\s]+Episode\s(\d+)')
+    for file in os.listdir(path):
+        match = regex.search(file)
+        if match is not None:
+            #shutil.move(file, 'Critical Role - S01E{:0>2} - {}.mp3'.format(match.group(2), match.group(1)))
+            print('Critical Role - S01E{:0>2} - {}.mp3'.format(match.group(2), match.group(1).title()))
+
+
+
+if __name__ == '__main__':
+    main()
